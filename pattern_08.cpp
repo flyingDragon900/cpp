6ed0578254e16d660667b9353ea857d8992a3b31
Dragon <bhadurmister@gmail.com>
2022-07-22 18:55:38 +0530

    initual commit

---
+++ pattern_08.cpp
@@ -0,0 +1,41 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int n;
+//   cin >> n;
+
+//   int i = 1;
+//   char ch = 'A';
+//   while (i <= n) {
+//     int j = 1;
+//     while (j <= n) {
+//       cout << ch;
+//       j++;
+//     }
+//     cout << endl;
+//     ch++;
+//     i++;
+//   }
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int i = 1;
+
+  while (i <= n) {
+    char ch = 'A' + i - 1;
+    int j = 1;
+    while (j <= n) {
+      cout << ch;
+      j++;
+    }
+    cout << endl;
+    i++;
+  }
+}
